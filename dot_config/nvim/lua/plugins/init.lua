e4298e30047961fcca2ab66f79da5fc3f2814b74
mohansella <rkssmohankumar@gmail.com>
2024-11-21 23:53:13 +0530

    switched to nvchad 2.5

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+  }
+}
