e36e864bd54df00d6aa0b7d9f0a3bf601db55fd8
mohansella <rkssmohankumar@gmail.com>
2024-11-30 22:10:38 +0530

    lspkind added to vscode like completion list in nvim

--- dot_config/nvim/lua/plugins/completion.lua
+++ dot_config/nvim/lua/plugins/completion.lua
@@ -11,11 +11,15 @@ local M = {
 
       --snippets
       'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip'
+      'saadparwaiz1/cmp_luasnip',
+
+      --format
+      'onsails/lspkind.nvim'
     },
 
     config = function()
-      local cmp = require'cmp'
+      local cmp = require('cmp')
+      local lspkind = require('lspkind')
 
       cmp.setup({
       snippet = {
@@ -31,6 +35,28 @@ local M = {
       window = {
         -- completion = cmp.config.window.bordered(),
         -- documentation = cmp.config.window.bordered(),
+      },
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = lspkind.cmp_format({
+      mode = 'symbol', -- show only symbol annotations
+      maxwidth = {
+        -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- menu = function() return math.floor(0.45 * vim.o.columns) end,
+        menu = 40, -- leading text (labelDetails)
+        abbr = 40, -- actual suggestion item
+      },
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function (_, vim_item)
+        -- ...
+        return vim_item
+      end
+    })
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
