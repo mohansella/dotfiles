9241b3ac6b774e85c7a725768e76c7b3afdb053d
mohansella <rkssmohankumar@gmail.com>
2025-10-14 10:45:08 +0530

    smooth scroll and cursor using cinnamon plugin

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,40 +1,58 @@
-local M = {
-    {
-        "catppuccin/nvim",
-        priority = 1000,
-        opts = {
-            mason = true,
-            noice = true,
-            notify = true,
-            which_key = true
-        },
-        config = function(_, opts)
-            require("catppuccin").setup(opts)
-            vim.cmd("colorscheme catppuccin")
-        end
+local m = {
+  {
+    "catppuccin/nvim",
+    priority = 1000,
+    opts = {
+      mason = true,
+      noice = true,
+      notify = true,
+      which_key = true
     },
-    {
-        'rcarriga/nvim-notify',
-        config = function(_, opts)
-            require('notify').setup(opts)
-        end
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd("colorscheme catppuccin")
+    end
+  },
+  {
+    'rcarriga/nvim-notify',
+    config = function(_, opts)
+      require('notify').setup(opts)
+    end
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      'rcarriga/nvim-notify',
     },
-    {
-        "folke/noice.nvim",
-        dependencies = {
-            'rcarriga/nvim-notify',
-        },
-        config = function(_, opts)
-            require('noice').setup(opts)
-        end
+    config = function(_, opts)
+      require('noice').setup(opts)
+    end
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function(_, opts)
+      require('gitsigns').setup(opts)
+    end
+  },
+  {
+    --smooth scrolling
+    "declancm/cinnamon.nvim",
+    opts = {
+      keymaps = {
+        basic = true,
+        extra = true,
+      },
+      options = {
+        max_delta = {
+          time = 100
+        }
+      }
     },
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function(_, opts)
-            require('gitsigns').setup(opts)
-        end
-    }
+    config = function(_, opts)
+      require("cinnamon").setup(opts)
+    end
+  }
 }
 
 
-return M
+return m
