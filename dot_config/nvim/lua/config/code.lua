76fa32e6e5bcb1007890dcdd0ab0d941c11926cb
mohansella <rkssmohankumar@gmail.com>
2025-10-30 00:15:47 +0530

    added dartls back since mason recommends it

--- dot_config/nvim/lua/config/code.lua
+++ dot_config/nvim/lua/config/code.lua
@@ -4,6 +4,12 @@ require("mason-lspconfig").setup({
   automatic_installation = true
 })
 
+-- Set up lspconfig.
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+vim.lsp.config("dartls", { capabilities = capabilities }) -- dart
+vim.lsp.enable({"dartls"})
+
 -- treesitter config
 require('nvim-treesitter.configs').setup({
   --Supported languages: https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
