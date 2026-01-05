0d8b36e51a633a0ba0c00a4e131480bdb27dcf3f
mohansella <rkssmohankumar@gmail.com>
2026-01-05 16:13:18 +0530

    fixed telescope file open due to large single line file

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,18 +1,25 @@
 local M = {
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope-ui-select.nvim',
-            'folke/noice.nvim',
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope-ui-select.nvim',
+      'folke/noice.nvim',
+    },
+    opts = {
+      defaults = {
+        preview = {
+          filesize_limit = 0.05,
         },
-        config = function(_, opts)
-            local ts = require('telescope')
-            ts.setup(opts)
-            ts.load_extension("ui-select")
-            ts.load_extension("noice")
-        end
-    }
+      },
+    },
+    config = function(_, opts)
+      local ts = require('telescope')
+      ts.setup(opts)
+      ts.load_extension("ui-select")
+      ts.load_extension("noice")
+    end
+  }
 }
 
 return M
